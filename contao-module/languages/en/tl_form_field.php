7226b76f1d10d40b30900a57cef0bf1ba89e60ca
Cliff Parnitzky <github@cliff-parnitzky.de>
2017-08-31 22:50:29 +0200

    Provide english translation

---
+++ contao-module/languages/en/tl_form_field.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_form_field']['hofff_formtools_help_legend']
+	= 'Help text';
+$GLOBALS['TL_LANG']['tl_form_field']['hofff_formtools_addHelp']
+	= array('Add help text', '');
+$GLOBALS['TL_LANG']['tl_form_field']['hofff_formtools_help']
+	= array('Help text', '');
