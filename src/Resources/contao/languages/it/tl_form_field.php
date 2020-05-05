4ed0f6f3ef95ada9dc73419548b505adea31ec27
PaoloB <paolob@contaocms.it>
2020-05-05 15:06:29 +0200

    Italian translation
    Added Italian translation

---
+++ src/Resources/contao/languages/it/tl_form_field.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_form_field']['hofff_formtools_help_legend']
+	= 'Spiegazione';
+$GLOBALS['TL_LANG']['tl_form_field']['hofff_formtools_addHelp']
+	= array('Aggiungi spiegazione', '');
+$GLOBALS['TL_LANG']['tl_form_field']['hofff_formtools_help']
+	= array('Spiegazione', '');
